5ef7b42b48c5df228c03caa0b5343fc8ed775663
Lesummer <144891846+TailorCornedge@users.noreply.github.com>
2024-04-12 09:38:01 +0200

    Add files via upload

--- Grind/alma.js
+++ Grind/alma.js
@@ -1,20 +1,6 @@
-document.getElementById('subscribeButton').addEventListener('click', function() {
-    // Első és második név ellenőrzése
-    var firstName = document.querySelector('.form__firstname').value.trim();
-    var lastName = document.querySelector('.form__lastname').value.trim();
-    if (firstName === '' || lastName === '') {
-        alert('Please enter your first and last name.');
-        return;
-    }
-
-    // Email ellenőrzése
-    var email = document.querySelector('.form__email').value.trim();
-    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailPattern.test(email)) {
-        alert('Please enter a valid email address.');
-        return;
-    }
-
-    // Feliratkozás végrehajtása (itt lehetne küldeni az adatokat egy szerverre, vagy máshova)
-    alert('You have successfully subscribed with the email: ' + email);
-});
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#image-slider', {
+          cover      : true,
+          heightRatio: 0.5,
+    } ).mount();
+  } );
